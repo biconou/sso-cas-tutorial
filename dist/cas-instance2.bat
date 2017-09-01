1fc73fcd4be1294c5b88f1203d35decbac1c2a2d
rcocula <remi.cocula@soprasteria.com>
2017-09-01 19:23:39 +0200

    WIP

--- dist/cas-instance2.bat
+++ dist/cas-instance2.bat
@@ -1,14 +1,21 @@
 
-del /f /q logs\instance2\*.*
+del /f /q %DIST_DIR%\logs\instance2\*.*
 
 set JAVA_HOME=D:\jdk1.8.0_65
 
 set DIST_DIR=D:\entorno\ide\workspace\sso-cas\dist
 set CAS_CONFIG_DIR=%DIST_DIR%\etc\cas\config\instance2
-set KEYSTORE_FILE=%DIST_DIR%\etc\cas\thekeystore.jks
+set KEYSTORE_FILE=%DIST_DIR%\etc\cas\thekeystore-cas-instance2.jks
 
 echo on
 
-%JAVA_HOME%\bin\java -jar cas.war ^
+%JAVA_HOME%\bin\java -jar %DIST_DIR%/../cas-overlay-template/target/cas.war ^
   --cas.standalone.config=%CAS_CONFIG_DIR% ^
-  --server.ssl.keyStore=file:%KEYSTORE_FILE%
+  --server.ssl.keyStore=file:%KEYSTORE_FILE% ^
+  --logging.config=file:%CAS_CONFIG_DIR%/log4j2.xml ^
+  --cas.ticket.registry.ehcache.configLocation=file:%CAS_CONFIG_DIR%/ehcache-replicated.xml ^
+  -Dcom.sun.management.jmxremote ^
+  -Dcom.sun.management.jmxremote.port=9010 ^
+  -Dcom.sun.management.jmxremote.local.only=false ^
+  -Dcom.sun.management.jmxremote.authenticate=false ^
+  -Dcom.sun.management.jmxremote.ssl=false
--- dist/genkey.bat
+++ dist/genkey.bat
@@ -3,6 +3,14 @@ set JAVA_HOME=D:\jdk1.8.0_65
 
 echo on
 
-del .\etc\cas\thekeystore.jks
+del .\etc\cas\thekeystore*.jks
 
-%JAVA_HOME%\bin\keytool -genkeypair -alias localhost -keyalg RSA -dname CN=localhost -validity 365 -keystore .\etc\cas\thekeystore.jks
+%JAVA_HOME%\bin\keytool -genkeypair -alias localhost -keyalg RSA -dname CN=localhost -validity 365 -keystore .\etc\cas\thekeystore-localhost.jks
+%JAVA_HOME%\bin\keytool -list -keystore .\etc\cas\thekeystore-localhost.jks
+
+%JAVA_HOME%\bin\keytool -genkeypair -alias cas-instance1 -keyalg RSA -dname CN=cas-instance1 -validity 365 -keystore .\etc\cas\thekeystore-cas-instance1.jks
+%JAVA_HOME%\bin\keytool -list -keystore .\etc\cas\thekeystore-cas-instance1.jks
+
+%JAVA_HOME%\bin\keytool -genkeypair -alias cas-instance2 -keyalg RSA -dname CN=cas-instance2 -validity 365 -keystore .\etc\cas\thekeystore-cas-instance2.jks
+%JAVA_HOME%\bin\keytool -list -keystore .\etc\cas\thekeystore-cas-instance2.jks
+
